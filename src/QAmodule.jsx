5cdb814b7ad47300c92bc02b01d787d960871fe5
urbanseabear <calvin.rudy@gmail.com>
2020-11-11 16:46:17 -0500

    qa module skeleton

---
+++ src/QAmodule.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class QAmodule extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: []
+    };
+  }
+  render() {
+    return (
+      <div>questions</div>
+    );
+  }
+}
